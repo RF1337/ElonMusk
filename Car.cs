b36cd932304961642c3c9fba89c66b4c7bb8fc93
RF1337 <112627787+RF1337@users.noreply.github.com>
2023-02-06 12:07:07 +0100

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ElonMusk
+{
+    class Car
+    {
+        //The variable that is used for determining if the car is started/turned on or not
+        public bool started = false;
+
+        //The variable that makes it possible to give the cars a colour
+        private string _colour;
+        public string Colour
+        {
+            get { return _colour; }
+            set { _colour = value; }
+        }
+
+        //The variable used to determine how much battery you have left
+        private byte _battery;
+        public byte Battery
+        {
+            get { return _battery; }
+            set { _battery = value; }
+        }
+
+        //The variable used to determine how far you have driven.
+        private int _distance;
+        public int Distance
+        {
+            get { return _distance; }
+            set { _distance = value; }
+        }
+
+        //Method that gets used to determine if the car can drive or not, if this method has been executed
+        //then it can start.
+        public void Start()
+        {
+            started = true;
+        }
+
+        //Method that stops the car/turns it off, if the car has been stopped - then you won't be able to use 
+        //the drive method.
+        public void Stop()
+        {
+            started = false;
+        }
+
+
+        //Method that drains the battery and adds the amount of metres driven per time it's executed
+        public void Drive()
+        {
+            //It only drains battery and adds distance if the car has been started.
+            if (started == true)
+            {
+                _battery--;
+                _distance += 20;
+            }
+            else
+            {
+                Console.WriteLine("You need to start the car.");
+            }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ElonMusk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating our car objects
+            Car car = new Car();
+            Car car2 = new Car();
+            Car car3 = new Car();
+
+            //Giving a colour to our cars
+            car.Colour = "Yellow";
+            car2.Colour = "Red";
+            car3.Colour = "Green";
+            string carColour = car.Colour;
+            string car2Colour = car2.Colour;
+            string car3Colour = car3.Colour;
+
+            //Giving the battery a start value of 100 for all cars
+            car.Battery = 100;
+            car2.Battery = 100;
+            car3.Battery = 100;
+            byte carBattery = car.Battery;
+            byte car2Battery = car2.Battery;
+            byte car3Battery = car3.Battery;
+
+            //Starting the cars so they can drive, if this wasn't done then they 
+            //wouldn't be able to use the drive method
+            car.Start();
+            car2.Start();
+            car3.Start();
+
+            //Creating a ReadLine that will determine how many times you wanna use the drive method
+            Console.WriteLine("How many times do you wanna drive?");
+            byte howManyTimes = byte.Parse(Console.ReadLine());
+
+            //Loop that executes the method as many times as the user has typed in the ReadLine above
+            for (int i = 0; i < howManyTimes; i++)
+            {
+                car.Drive();
+                car2.Drive();
+                car3.Drive();
+            }
+            //Prints in the console how much battery each car has remaining
+            Console.WriteLine($"Car has {car.Battery}% battery. Car 2 has {car2.Battery}% " +
+                $"battery & car 3 has {car3.Battery}% battery.");
+
+            //Prints in the console how far each car has driven
+            Console.WriteLine($"Car has driven {car.Distance} metres. Car 2 has driven {car2.Distance} " +
+                $"metres & car 3 has driven {car3.Distance} metres");
+        }
+    }
+}
+
