dad984292123e6a1ccdd0d4cbca3c95830831489
RF1337 <112627787+RF1337@users.noreply.github.com>
2023-02-06 10:55:03 +0100

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ElonMusk
+{
+    class Car
+    {
+        public bool started = false;
+
+        private string _colour;
+
+        public string Colour
+        {
+            get { return _colour; }
+            set { _colour = value; }
+        }
+
+        private byte _battery;
+
+        public byte Battery
+        {
+            get { return _battery; }
+            set { _battery = value; }
+        }
+
+        private int _distance;
+
+        public int Distance
+        {
+            get { return _distance; }
+            set { _distance = value; }
+        }
+
+        public void Start()
+        {
+            started = true;
+        }
+
+        public void Stop()
+        {
+            started = false;
+        }
+
+        public void Drive()
+        {
+            if (started == true)
+            {
+                _battery--;
+                _distance += 20;
+            }
+            else
+            {
+                Console.WriteLine("You need to start the car.");
+            }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ElonMusk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            Car car2 = new Car();
+            Car car3 = new Car();
+
+            car.Colour = "Yellow";
+            car2.Colour = "Red";
+            car3.Colour = "Green";
+            string carColour = car.Colour;
+            string car2Colour = car2.Colour;
+            string car3Colour = car3.Colour;
+
+            car.Battery = 100;
+            car2.Battery = 100;
+            car3.Battery = 100;
+            byte carBattery = car.Battery;
+            byte car2Battery = car2.Battery;
+            byte car3Battery = car3.Battery;
+
+            car.Start();
+            car2.Start();
+            car3.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                car.Drive();
+                car2.Drive();
+                car3.Drive();
+            }
+            Console.WriteLine($"Car has {car.Battery}% battery. Car 2 has {car2.Battery}% " +
+                $"battery & car 3 has {car3.Battery}% battery.");
+
+            Console.WriteLine($"Car has driven {car.Distance} metres. Car 2 has driven {car2.Distance} " +
+                $"metres & car 3 has driven {car3.Distance} metres");
+        }
+    }
+}
+
